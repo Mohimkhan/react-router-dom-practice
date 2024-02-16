cdf2c7f44937540092cd8d52e1271dba981587c0
Mohimkhan <mohimkhan652@gmail.com>
2024-02-16 22:13:56 +0600

    feat: All the routes and their loader, actions

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./routes/root";
+import ErrorPage from "./ErrorPage";
+import Contact from "./routes/contact";
+import { ContactLoader } from "./components/Loader";
+import { contactAction } from "./actions/contactAction";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: ContactLoader,
+    action: contactAction,
+    children: [
+      {
+        path: "/contacts/:contactId",
+        element: <Contact />,
+      },
+    ],
+  },
+  {
+    path: "/about",
+    element: <div>About Us</div>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
