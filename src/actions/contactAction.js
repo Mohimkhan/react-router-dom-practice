5df92ec2b8af5a0501fdbd7b25b3a4b7ece3297a
Mohimkhan <mohimkhan652@gmail.com>
2024-02-17 16:28:25 +0600

    Add action for edit/update

--- src/actions/contactAction.js
+++ src/actions/contactAction.js
@@ -1,6 +1,14 @@
-import { createContact } from "../contacts";
+import { redirect } from "react-router-dom";
+import { createContact, updateContact } from "../contacts";
 
 export async function contactAction() {
   const contact = await createContact();
   return { contact };
+}
+
+export async function contactUpdateAction({ request, params }) {
+  const formData = await request.formData();
+  const updates = Object.fromEntries(formData);
+  await updateContact(params.contactId, updates);
+  return redirect(`/contacts/${params.contactId}`);
 }
